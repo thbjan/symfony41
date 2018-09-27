db92cc8cbbc7ecbf32a932de964c5da4fb6f3db7
Thomas Andersen <thomas.bjerregaard.andersen@gmail.com>
2018-09-27 02:56:42 +0200

    Twig template added

--- src/Controller/ToDoController.php
+++ src/Controller/ToDoController.php
@@ -3,13 +3,18 @@
 	use Symfony\Component\HttpFoundation\Response;
 	use Symfony\Component\Routing\Annotation\Route;
   	use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
-	class ToDoController {
+  	use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+  	// class ToDoController {  
+	class ToDoController extends Controller{  
 		/**
 		* @Route("/", name="to_do_index")
 		* @Method({"GET"})
 		*/
 		public function index() {
-			return new Response('<html><body><h1>TEST</h1></body></html>');
+			// return new Response('<html><body><h1>TEST</h1></body></html>');
+			return $this->render('todo/index.html.twig');
+
 		}
 	} 
 ?>
