03a17ea97c4954de12d18d3d09290c2af652f6bd
Thomas Andersen <thomas.bjerregaard.andersen@gmail.com>
2018-09-28 03:21:59 +0200

    Hard-coded styles removed
    The hard-coded styles removed,  missing JQuery added. Considering to use popper.js as tooltip for todo content when hover over todos (not good on mobile devices)

--- src/Controller/ToDoController.php
+++ src/Controller/ToDoController.php
@@ -6,6 +6,12 @@
   	use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
   	use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
+	use Symfony\Component\Form\Extension\Core\Type\TextType;
+	use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+	use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+
+	use Symfony\Component\HttpFoundation\Request;
+
   	// class ToDoController {  
 	class ToDoController extends Controller{  
 		/**
@@ -29,6 +35,15 @@
 		public function show($id) {
 			$todo = $this->getDoctrine()->getRepository(Todo::class)->find($id);
 			return $this->render('todo/show.html.twig', ['todo' => $todo]); 
+		}
+
+		/**
+		* @Route("/todo/add", name="to_do_add")
+		* Method({"GET", "POST"})
+		*/
+		public function add(Request $request) {
+			$todo = new Todo();
+			
 		}
 
 		
